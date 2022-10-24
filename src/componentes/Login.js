c7513c77f7a7000939e4f1076fedd2bf23cad17a
CamilaPaiz <camilapaiz@hotmail.com>
2022-10-24 20:51:34 -0300

    requisitos-Login-finalizados

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import RecipeContext from '../context/RecipeContext';
 
 export default function Login() {
+  const { email,
+    password,
+    handleEmail,
+    handlePassword,
+    handleClick,
+    isDisabled,
+  } = useContext(RecipeContext);
+
   return (
     <div>
       <input
         data-testid="email-input"
         placeholder="E-mail"
+        value={ email }
+        onChange={ handleEmail }
       />
 
       <input
         data-testid="password-input"
         placeholder="Password"
+        value={ password }
+        onChange={ handlePassword }
       />
 
       <button
         data-testid="login-submit-btn"
         type="button"
+        disabled={ isDisabled }
+        onClick={ handleClick }
       >
         Enter
       </button>
---
+++ src/context/RecipeProvider.js
@@ -0,0 +1,70 @@
+import React, { useState, useMemo, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import RecipeContext from './RecipeContext';
+
+function RecipeProvider({ children }) {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const verifyBtn = () => {
+      const validEmail = /\S+@\S+\.\S+/;
+      const verifyEmail = validEmail.test(email);
+      const numeroMin = 6;
+      const verifyPassword = password.length > numeroMin;
+      if ((verifyPassword && verifyEmail)) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+    };
+    verifyBtn();
+  }, [email, password]);
+
+  const handleEmail = ({ target }) => {
+    setEmail(target.value);
+    /*  verifyBtn(); */
+  };
+
+  const handlePassword = ({ target }) => {
+    setPassword(target.value);
+    /*  verifyBtn(); */
+  };
+
+  const handleClick = () => {
+    localStorage.setItem('user', JSON.stringify({ email }));
+    history.push('/meals');
+  };
+
+  const context = useMemo(() => ({
+    email,
+    password,
+    isDisabled,
+    handleEmail,
+    handlePassword,
+    /*  verifyBtn, */
+    handleClick,
+  }), [
+    email,
+    password,
+    isDisabled,
+    handleEmail,
+    handlePassword,
+    /*  verifyBtn, */
+    handleClick,
+  ]);
+
+  return (
+    <RecipeContext.Provider value={ context }>
+      { children }
+    </RecipeContext.Provider>
+  );
+}
+
+RecipeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+export default RecipeProvider;
