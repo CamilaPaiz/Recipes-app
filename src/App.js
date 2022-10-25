a0f6805fddf629d9e57f9de0acf817c03b8c253d
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2022-10-25 15:55:01 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import RecipeProvider from './context/RecipeProvider';
 import Login from './componentes/Login';
+import Header from './componentes/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <RecipeProvider>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Login } />
+            <Route path="/meals" component={ App } />
+            <Route path="/drinks" component={ App } />
+            <Route path="/meals/:id-da-receita" component={ App } />
+            <Route path="/drinks/:id-da-receita" component={ App } />
+            <Route path="/meals/:id-da-receita/in-progress" component={ App } />
+            <Route path="/drinks/:id-da-receita/in-progress" component={ App } />
+            <Route path="/profile" component={ App } />
+            <Route path="/done-recipes" component={ App } />
+            <Route path="/favorite-recipes" component={ App } />
           </Switch>
         </BrowserRouter>
       </RecipeProvider>
---
+++ src/componentes/Header.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Header() {
+  const [input, setInput] = useState(false);
+  const click = (result) => {
+    if (result === true) {
+      setInput(false);
+    } else {
+      setInput(true);
+    }
+  };
+  let titulo;
+  const history = useHistory();
+  const { location } = history;
+  const localizacao = location.pathname.substring(1);
+  const done = 'done-recipes';
+  const favoriteRecipes = 'favorite-recipes';
+  if (localizacao !== '') {
+    titulo = localizacao[0].toUpperCase() + localizacao.substring(1);
+  }
+  if (localizacao === done) {
+    titulo = 'Done Recipes';
+  } else if (localizacao === favoriteRecipes) {
+    titulo = 'Favorite Recipes';
+  }
+  return (
+    <div>
+      {localizacao === 'meals'
+        || localizacao === 'drinks'
+        || localizacao === 'profile'
+        || localizacao === done || localizacao === favoriteRecipes ? (
+          <div>
+            <h2 data-testid="page-title">{titulo !== undefined && titulo}</h2>
+            <button
+              type="button"
+              onClick={ () => history.push('/profile') }
+            >
+              <img
+                data-testid="profile-top-btn"
+                src="../images/profileIcon.svg"
+                alt="as"
+              />
+            </button>
+            {localizacao !== 'profile'
+            && localizacao !== done && localizacao !== favoriteRecipes
+              ? (
+                <button type="button" onClick={ () => { click(input); } }>
+                  <img
+                    data-testid="search-top-btn"
+                    src="../images/searchIcon.svg"
+                    alt="aas"
+                  />
+                </button>
+              ) : null}
+          </div>
+        ) : null}
+      {input === true && (
+        <div>
+          <input type="text" data-testid="search-input" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
