cfeceae3e1b7dabf7d2c0829cbc0f13589dd318b
CamilaPaiz <camilapaiz@hotmail.com>
2022-10-27 22:14:40 -0300

    iniciando requisitos recipes details e finalizando 24

--- src/componentes/RecipeDetails.js
+++ src/componentes/RecipeDetails.js
@@ -7,64 +7,124 @@ export default function RecipeDetails() {
   const params = useParams();
   console.log(params.id);
   // const [detailsIDRecipe, setDetailsIDRecipe] = useState([]);
-  const [detailsIDMeals, setDetailsIDMeals] = useState();
-  const [detailsIDDrinks, setDetailsIDDrinks] = useState();
+  const [detailsIDMeals, setDetailsIDMeals] = useState([]);
+  const [detailsIDDrinks, setDetailsIDDrinks] = useState([]);
+  /* const [drinkRoute, setdrinkRoute] = useState(false);
+  const [mealsRoute, setmealsRoute] = useState(false); */
 
   const requestdetailsRecipe = async () => {
     let endpointdetailsId;
-    if (history.location.pathname === '/meals') {
+    const pathnameToCompare = history.location.pathname.split('/');
+    if (pathnameToCompare[1] === 'meals') {
       endpointdetailsId = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.id}`;
-      console.log(endpointdetailsId);
-    } else if (history.location.pathname === '/drinks') {
+      /* setmealsRoute(true); */
+    } else if (pathnameToCompare[1] === 'drinks') {
       endpointdetailsId = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`;
+      /* setdrinkRoute(true); */
     }
     const response = await fetch(endpointdetailsId);
     const result = await response.json();
-    if (history.location.pathname === '/meals') {
+    if (pathnameToCompare[1] === 'meals') {
       setDetailsIDMeals(result);
-    } else if (history.location.pathname === '/drinks') {
+    } else if (pathnameToCompare[1] === 'drinks') {
       setDetailsIDDrinks(result);
     }
+    console.log(detailsIDDrinks);
+    console.log(detailsIDMeals);
   };
-  console.log(detailsIDMeals);
+
   useEffect(() => {
     requestdetailsRecipe();
   }, []);
 
-  /* if (history.location.pathname === '/meals') {
-    setDataCategoryMeals(result.meals.slice(0, CINCO));
-    console.log(dataCategoryMeals);
-  } else if (history.location.pathname === '/drinks') {
-    setDataCategoryDrinks(result.drinks.slice(0, CINCO));
-    console.log(dataCategoryDrinks);
-  } */
-
-  // console.log(detailsIDDrinks.idMeal);
-  /* const requestRecipe = async () => {
-    let endpointRecipe;
-    if (history.location.pathname === '/meals') {
-      endpointRecipe = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id - da - receita}`;
-    } else if (history.location.pathname === '/drinks') {
-      endpointRecipe = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id - da - receita}`;
+  const handleClick = () => {
+    const pathnameToCompare = history.location.pathname.split('/');
+    if (pathnameToCompare[1] === 'meals') {
+      history.push(`/meals/${params.id}/in-progress`);
+    } else if (pathnameToCompare[1] === 'drinks') {
+      history.push(`/drinks/${params.id}/in-progress`);
     }
-    const response = await fetch(endpointRecipe);
-    const result = await response.json();
-    return result;
-  }; */
-
-  /* useEffect(() => {
-    requestRecipe();
-  }, []); */
+  };
 
   return (
     <div>
-      <h2 data-testid="recipe-title">Título receita</h2>
-      <img data-testid="recipe-photo" alt="imagem receita" />
-      <p data-testid="recipe-category">texto catergoria</p>
-      {/*  <p data-testid="${index}-ingredient-name-and-measure">ingredientes</p> */}
-      <p data-testid="instructions">texto instrução</p>
-      <p data-testid="video" />
+      {/* {
+        (drinkRoute) && detailsIDDrinks.map((item, index) => (
+          <div key={ index }>
+            <img
+              src={ item.strMealThumb }
+              alt={ item.strMeal }
+              data-testid="recipe-photo"
+              style={ { width: '200px', height: '150px' } }
+            />
+            <h3 data-testid="recipe-title">{item.strMeal}</h3>
+            <p data-testid="recipe-category">{item.strCategory}</p>
+            <p data-testid="instructions">{item.strInstructions}</p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient1}
+
+            </p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient2}
+
+            </p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient3}
+
+            </p>
+
+          </div>
+        ))
+      } */}
+      {/*  {
+        (mealsRoute) && detailsIDMeals.map((item, index) => (
+          <div key={ index }>
+            <img
+              src={ item.strMealThumb }
+              alt={ item.strMeal }
+              data-testid="recipe-photo"
+              style={ { width: '200px', height: '150px' } }
+            />
+            <h3 data-testid="recipe-title">{item.strMeal}</h3>
+            <p data-testid="recipe-category">{item.strCategory}</p>
+            <p data-testid="instructions">{item.strInstructions}</p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient1}
+
+            </p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient2}
+
+            </p>
+            <p
+              data-testid={ `${index}-ingredient-name-and-measure` }
+            >
+              {item.strIngredient3}
+
+            </p>
+            <p>{item.strVideo}</p>
+
+          </div>
+        ))
+      } */}
+      <button type="button" data-testid="favorite-btn">
+        Favorite
+      </button>
+      <button type="button" data-testid="share-btn">
+        Share
+      </button>
       <button
+        onClick={ handleClick }
         className="recipesDetails"
         data-testid="start-recipe-btn"
         type="button"
