e6b495ebd0fcf6e7c9d37f1863d1c0b56dc0267f
CamilaPaiz <camilapaiz@hotmail.com>
2022-10-29 16:27:24 -0300

    requisito 37

--- src/componentes/ProgressMeals.js
+++ src/componentes/ProgressMeals.js
@@ -1,10 +1,12 @@
 import Header from './Header';
+import RecipeInProgress from './RecipeInProgress';
 
 function ProgressMeals() {
   return (
     <div>
       <Header />
       <h1>Teste ProgressMeals</h1>
+      <RecipeInProgress />
     </div>
   );
 }
