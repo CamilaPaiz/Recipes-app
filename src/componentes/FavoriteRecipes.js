8a9ff8b2c269629423375755fb29e8d6e50584d1
Carlos Roberto <carlosrobertocostajunior@id.uff.br>
2022-11-03 19:46:00 -0300

    requisito 50 implementado

--- src/componentes/FavoriteRecipes.js
+++ src/componentes/FavoriteRecipes.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import Header from './Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 function FavoriteRecipes() {
   return (
-    <div>
-      <Header />
-      <h1>Teste FavoriteRecipes</h1>
-    </div>
+    <>
+      <div className="favoriterecipe">
+        <Header />
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+          >
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-meal-btn"
+          >
+            Food
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+          >
+            Drinks
+          </button>
+        </div>
+      </div>
+      <img
+        data-testid={ `${index}-horizontal-image` }
+        alt="foto"
+      />
+      <span data-testid={ `${index}-horizontal-top-text` } />
+      <span data-testid={ `${index}-horizontal-name` } />
+      <button
+        type="button"
+      >
+        <img
+          src={ blackHeartIcon }
+          alt="Favorite Button"
+          data-testid={ `${index}-horizontal-favorite-btn` }
+        />
+      </button>
+      <button type="button" data-testid={ `${index}-horizontal-share-btn` }>
+        <img src={ shareIcon } alt="shareicon" />
+      </button>
+    </>
   );
 }
-
 export default FavoriteRecipes;
